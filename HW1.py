a0c8b428407ef6a059a7546cfaafd7c0b3f9c286
Afonso <afonso.f.rosa@gmail.com>
2024-09-20 23:33:52 +0100

    .

--- HW1.py
+++ HW1.py
@@ -3,10 +3,15 @@ import pandas as pd
 from scipy.io.arff import loadarff
 from sklearn.feature_selection import f_classif
 
-path = "/home/afonso215/Aprendizagem/diabetes.arff"
+path = "diabetes.arff"
 
 data = loadarff(path)
 df = pd.DataFrame(data[0])
 
+df['Outcome'] = df['Outcome'].str.decode('utf-8')
 
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
+
+fimportance = f_classif(X, y)
 
